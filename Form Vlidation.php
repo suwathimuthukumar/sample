10d0cf063126e980ec5e7707164e5a2d3d951cea
suwathimuthukumar <156401705+suwathimuthukumar@users.noreply.github.com>
2024-03-14 11:02:44 +0530

    Add files via upload

---
+++ Form Vlidation.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FORM</title>
+</head>
+<body>
+    <form action="./dummy.php" method="post" autocomplete="off">
+        <lable for="username">Name:</lable>
+        <input type="text" id="username" name="username">
+        <br><br>
+        <lable for="email">Email:</lable>
+        <input type="text" id="email" name="email">
+        <br><br>
+        <button>click</button>
+</body>
+</html>
---
+++ dummy.php
@@ -0,0 +1,3 @@
+<h1>Welcom your successfully registered...👍</h1>
+    <p>Name:<?php echo $_POST ["username"]?></p>
+    <p>Email:<?php echo $_POST ["email"]?></p>
